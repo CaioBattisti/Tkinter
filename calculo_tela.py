7a7c0f821d494e55972b5e0e0dcea061018de0ed
CaioBattisti <cluizbattisti@gmail.com>
2025-02-24 20:22:22 -0300

    atualização

--- calculo_tela.py
+++ calculo_tela.py
@@ -12,7 +12,7 @@ i.geometry('980x720+250+30')
 lb=Label(i,text="0")
 lb.place(x=230,y=200)
 
-bt=Button(i,width="20",text='calcular',command=bt_click)
+bt=Button(i,width="20",text='calcular +',command=bt_click)
 bt.place(x=230,y=230)
 
 ed1 = Entry(i)
@@ -21,4 +21,15 @@ ed1.place(x=230,y=150)
 ed2 = Entry(i)
 ed2.place(x=230,y=180)
 
+
 i.mainloop()
+
+#def bt_click():
+#    num1 = int(ed1.get())
+#    num2 = int(ed2.get())
+#    lb["text"] = num1 - num2
+
+#def bt_click():
+#    num1 = int(ed1.get())
+#    num2 = int(ed2.get())
+#    lb["text"] = num1 * num2
