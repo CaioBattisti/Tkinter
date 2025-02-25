bbe2e8e5733f3865bb17879d8089f299682fb324
CaioBattisti <cluizbattisti@gmail.com>
2025-02-25 19:37:39 -0300

    save

---
+++ desafio1.py
@@ -0,0 +1,39 @@
+from tkinter import *
+i=Tk()
+i.title("Programa Financeiro")
+i.geometry('980x720+250+30')
+
+'''lb1=Label(i,text="Login:",bg="yellow")
+#componente .grid serve tambem para posicionar utilizando indicativo de linha(row) e coluna (column).
+
+lb1.grid(row=1,column=1)
+
+lb2=Label(i,text="Senha:",bg="red")
+lb2.grid(row=9,column=1)
+
+ed1 = Entry(i)
+ed1.grid(row=3,column=1)
+
+ed2 = Entry(i)
+ed2.grid(row=10,column=1)
+
+bt1 = Button(i,text="Login")
+bt1.grid(row=11,column=1)'''
+
+#o codigo abaixo faz coreção das posições das labels,caixa de texto e botao
+lb1=Label(i,text="Login:",bg="yellow")
+lb1.grid(row=1,column=1)
+
+lb2=Label(i,text="Senha:",bg="red")
+lb2.grid(row=2,column=1)
+
+ed1 = Entry(i)
+ed1.grid(row=1,column=2)
+
+ed2 = Entry(i)
+ed2.grid(row=2,column=2)
+
+bt1 = Button(i,text="Login")
+bt1.grid(row=3,column=1)
+
+i.mainloop()
