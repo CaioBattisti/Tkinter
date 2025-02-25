a0b731a91750f1e2797f8738ba227ad0042780a1
CaioBattisti <cluizbattisti@gmail.com>
2025-02-25 16:36:01 -0300

    save

--- tkinter_anchor.py
+++ tkinter_anchor.py
@@ -4,7 +4,7 @@ i.title("Programa Financeiro")
 i.geometry('980x720+250+30')
 
 lb1 = Label(i,text='Label 1',bg="yellow")
-lb1.place(x=230,y=200)
+lb1.place(x=300,y=200)
 
 lb2 = Label(i,text='Label 2',bg="pink")
 lb2.place(x=230,y=200)
