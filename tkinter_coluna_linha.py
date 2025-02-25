a37f5e12eeef6679a6940b77f4264b092df6b2f9
CaioBattisti <cluizbattisti@gmail.com>
2025-02-25 19:11:40 -0300

    save

--- tkinter_coluna_linha.py
+++ tkinter_coluna_linha.py
@@ -6,17 +6,17 @@ i.geometry('980x720+250+30')
 lb1=Label(i,text="Login",bg="yellow")
 #componente .grid serve tambem para posicionar utilizando indicativo de linha(row) e coluna (column).
 
-lb1.grid(row=1,column=2)
-lb1.grid(row=1,column=2)
+lb1.grid(row=1,column=7)
+lb1.grid(row=1,column=7)
 
 lb2=Label(i,text="Senha",bg="red")
-lb2.grid(row=9,column=2)
+lb2.grid(row=9,column=7)
 
 ed1 = Entry(i)
-ed1.grid(row=3,column=2)
+ed1.grid(row=3,column=7)
 
 ed2 = Entry(i)
-ed2.grid(row=10,column=2)
+ed2.grid(row=10,column=7)
 
 bt1 = Button(i,text="Login")
 bt1.grid(row=11,column=2)
