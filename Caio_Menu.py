96867c301afcef62ce11655f3d6ecc2088aefca5
CaioBattisti <cluizbattisti@gmail.com>
2025-02-27 20:58:00 -0300

    save

--- Caio_Menu.py
+++ Caio_Menu.py
@@ -1,13 +1,14 @@
 #exercicio1
 from tkinter import *
+
 class Janela:
     def __init__(self, instancia_de_Tk):
-        principal = Menu(instancia_de_Tk)
-        arquivo =Menu(principal)
-        arquivo.add_command(Label="Abrir",command=self.abrir)
-        arquivo.add_command(Label="Salvar",command=self.salvar)
-        principal.add_command(Label="Arquivo",command=self.arquivo)
-        principal.add_command(Label="Ajuda",command=self.ajuda)
+        principal =Menu(instancia_de_Tk)
+        arquivo=Menu(principal)
+        arquivo.add_command(label="Abrir",command=self.abrir)
+        arquivo.add_command(label="Salvar",command=self.salvar)
+        principal.add_cascade(label="Arquivo",menu=arquivo)
+        principal.add_command(label="Ajuda",command=self.ajuda)
         instancia_de_Tk.config(menu=principal)
     def abrir(self):
         print("abrir")
